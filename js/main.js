aa914b0f2599dfbde99c7eea66f46287a4c7ced2
Efe Erdem <efeerdem777@hotmail.com>
2023-05-08 13:08:57 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,218 @@
+$(document).ready(function() {
+    var owl_1 = $('.owl_1');
+    owl_1.owlCarousel({
+        center: true,
+        nav: true,
+        margin: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.owl_1_next').click(function() {
+        owl_1.trigger('next.owl.carousel');
+    });
+    $('.owl_1_prev').click(function() {
+        owl_1.trigger('prev.owl.carousel');
+    });
+
+    var owl_2 = $('.owl_2');
+    owl_2.owlCarousel({
+        center: true,
+        nav: true,
+        margin: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.owl_2_next').click(function() {
+        owl_2.trigger('next.owl.carousel');
+    });
+    $('.owl_2_prev').click(function() {
+        owl_2.trigger('prev.owl.carousel');
+    });
+
+    var owl_3 = $('.owl_3');
+    owl_3.owlCarousel({
+        center: true,
+        nav: true,
+        margin: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.owl_3_next').click(function() {
+        owl_3.trigger('next.owl.carousel');
+    });
+    $('.owl_3_prev').click(function() {
+        owl_3.trigger('prev.owl.carousel');
+    });
+
+    var owl_4 = $('.owl_4');
+    owl_4.owlCarousel({
+        center: true,
+        nav: true,
+        margin: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.owl_4_next').click(function() {
+        owl_4.trigger('next.owl.carousel');
+    });
+    $('.owl_4_prev').click(function() {
+        owl_4.trigger('prev.owl.carousel');
+    });
+
+    var owl_5 = $('.owl_5');
+    owl_5.owlCarousel({
+        center: true,
+        nav: true,
+        margin: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.owl_5_next').click(function() {
+        owl_5.trigger('next.owl.carousel');
+    });
+    $('.owl_5_prev').click(function() {
+        owl_5.trigger('prev.owl.carousel');
+    });
+
+    var owl_6 = $('.owl_6');
+    owl_6.owlCarousel({
+        center: true,
+        nav: true,
+        margin: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.owl_6_next').click(function() {
+        owl_6.trigger('next.owl.carousel');
+    });
+    $('.owl_6_prev').click(function() {
+        owl_6.trigger('prev.owl.carousel');
+    });
+
+    var owl_7 = $('.owl_7');
+    owl_7.owlCarousel({
+        center: true,
+        nav: true,
+        margin: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.owl_7_next').click(function() {
+        owl_7.trigger('next.owl.carousel');
+    });
+    $('.owl_7_prev').click(function() {
+        owl_7.trigger('prev.owl.carousel');
+    });
+
+    var owl_8 = $('.owl_8');
+    owl_8.owlCarousel({
+        center: true,
+        nav: true,
+        margin: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.owl_8_next').click(function() {
+        owl_8.trigger('next.owl.carousel');
+    });
+    $('.owl_8_prev').click(function() {
+        owl_8.trigger('prev.owl.carousel');
+    });
+
+    var owl_9 = $('.owl_9');
+    owl_9.owlCarousel({
+        center: true,
+        nav: true,
+        margin: 0,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.owl_9_next').click(function() {
+        owl_9.trigger('next.owl.carousel');
+    });
+    $('.owl_9_prev').click(function() {
+        owl_9.trigger('prev.owl.carousel');
+    });
+
+});
---
+++ mail.php
@@ -0,0 +1,36 @@
+﻿<?php
+if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower(@$_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+    // Eğer Ajax işlemi dışında dosyaya erişim sağlanırsa hata verdirelim
+    die("Erişim engellendi!");
+}
+else{
+	if (@$_POST['e-mail']!='' && @$_POST['ad']!='' && @$_POST['soyad']!='' && @$_POST['tel']!='' && @$_POST['text']!='') {
+
+	require_once "mail/PHPMailerAutoload.php";
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->SMTPDebug = 1; 
+    $mail->SMTPAuth = true;
+    $mail->Host = 'smtp.yandex.com';
+    $mail->Port = 587;
+    $mail->SMTPSecure = 'tls';
+    $mail->IsHTML(true);
+    $mail->SetLanguage("tr", "phpmailer/language");
+    $mail->CharSet ="utf-8";
+    $mail->Username = "sender@digma.com.tr"; 
+    $mail->Password = "Parola1234";
+    $mail->SetFrom("sender@digma.com.tr", (@$_POST['ad'].' '.@$_POST['soyad'])); // Mail attigimizda yazacak isim
+    $mail->AddAddress("info@digma.com.tr"); // Maili gonderecegimiz kisi/ alici
+    $mail->Subject = 'Digma Yorum ->'.@$_POST['ad'].' '.@$_POST['soyad']; // Konu basligi
+    $mail->Body = @$_POST['text']."<br>".@$_POST['e-mail']."<br>".@$_POST['tel']; // Mailin icerigi
+    if(!$mail->Send())
+	{
+		echo '1';
+		exit;
+	}
+	echo '0';
+	} else {
+		 echo '2';
+	}
+}
+?>
